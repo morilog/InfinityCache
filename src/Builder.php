b2aad5b916dbd4a4c8e46f44a12827a7a0508458
Morteza Parvini <m.parvini@outlook.com>
2015-09-18 20:57:39 +0430

    initial commit

---
+++ src/Builder.php
@@ -0,0 +1,67 @@
+<?php
+namespace Morilog\InfinityCache;
+
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Database\Query\Grammars\Grammar;
+use Illuminate\Database\Query\Processors\Processor;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
+class Builder extends QueryBuilder
+{
+    /**
+     * @var Repository
+     */
+    private $cache;
+
+    /**
+     * @var
+     */
+    private $cacheTag;
+
+    /**
+     * @param Repository $cache
+     * @param ConnectionInterface $connection
+     * @param Grammar $grammar
+     * @param Processor $processor
+     * @param $cacheTag
+     */
+    public function __construct(
+        Repository $cache,
+        ConnectionInterface $connection,
+        Grammar $grammar,
+        Processor $processor,
+        $cacheTag
+    ) {
+        $this->cache = $cache;
+        parent::__construct($connection, $grammar, $processor);
+        $this->cacheTag = $cacheTag;
+    }
+
+    public function generateCacheKey()
+    {
+        return sha1($this->connection->getName() . $this->toSql() . serialize($this->getBindings()));
+    }
+
+    /**
+     * Execute the query as a "select" statement.
+     *
+     * @param  array $columns
+     * @return array|static[]
+     */
+    public function get($columns = ['*'])
+    {
+        $cacheKey = $this->generateCacheKey();
+
+        if (null === ($results = $this->cache->tags($this->cacheTag)->get($cacheKey))) {
+            $results = parent::get($columns);
+            $this->cache->tags($this->cacheTag)->forever($cacheKey, $results);
+        }
+
+        return $results;
+    }
+
+
+
+
+}
